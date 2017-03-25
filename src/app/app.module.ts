ff4b486c44647d345bfd088bbd166b762b972521
Parvesh <parveshh@gmail.com>
2017-03-25 19:47:46 +0000

    Intial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    HomeComponent
   ],
   imports: [
     BrowserModule,
